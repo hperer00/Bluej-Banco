8e93017c28a3624efbbed03d278a67565b623d99
hperer00 <hperer00@estudiantes.unileon.es>
2017-03-02 00:23:44 +0100

    Hecho

---
+++ Banco/Main.java
@@ -0,0 +1,24 @@
+
+/**
+ * Write a description of class Main here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Main
+{
+    public static void main(String args[])
+    {
+        Cliente yo=new Cliente("Hugo", 19);
+        
+        yo.abrirCuenta ();
+        
+        
+        
+        yo.ingresarDinero(3000);
+        
+        yo.sacarDinero(100);
+        
+        yo.consultarSaldo();
+    }
+}
